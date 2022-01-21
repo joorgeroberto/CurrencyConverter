6522b88cb8971b0bd7d738d5e89a1f982f29908e
Jorge de Carvalho <jorgercj@dcomp.ufs.br>
2022-01-21 19:50:31 -0300

    Initial Commit

---
+++ CurrencyConverter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CurrencyConverter
+//
+//  Created by Jorge de Carvalho on 21/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
